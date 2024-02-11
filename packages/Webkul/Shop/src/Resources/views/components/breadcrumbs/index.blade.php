80a02f8a073becb8d62c9db8923ece642d665587
Dikshant <dikshantjoshi97@gmail.com>
2024-02-11 15:22:24 +0000

    test commit 11/2/24

--- packages/Webkul/Shop/src/Resources/views/components/breadcrumbs/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/breadcrumbs/index.blade.php
@@ -3,7 +3,7 @@
     'entity' => null,
 ])
 
-<div class="flex justify-start mt-[5px] mb-[20px] max-lg:hidden">
+<div class="flex justify-start mt-[5px] mb-[20px] max-lg:hidden testclass">
     <div class="flex gap-x-[14px] items-center">        
         {{ Breadcrumbs::view('shop::partials.breadcrumbs', $name, $entity) }}
     </div>
