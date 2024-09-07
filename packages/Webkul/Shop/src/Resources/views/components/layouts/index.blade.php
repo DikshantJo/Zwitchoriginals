fc4dc97d12f0d47d0efde78d31562e87c49e7060
Dikshant Joshi <dikshantjoshi97@gmail.com>
2024-09-07 19:52:55 +0100

    pixel code added to common file in layout

--- packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
@@ -44,6 +44,7 @@
 
         @stack('styles')
 
+
         <style>
             {!! core()->getConfigData('general.content.custom_scripts.custom_css') !!}
             body {
@@ -271,6 +272,24 @@
 
         </style>
 
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2577968982375437');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=2577968982375437&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
+
         {!! view_render_event('bagisto.shop.layout.head') !!}
     </head>
 
@@ -337,5 +356,7 @@
         <script type="text/javascript">
             {!! core()->getConfigData('general.content.custom_scripts.custom_javascript') !!}
         </script>
+
+          
     </body>
 </html>
